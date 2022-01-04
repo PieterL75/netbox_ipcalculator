74ca81c1cf4d0183af03738c4f0c7c1e96a18edc
PieterL75 <74899468+PieterL75@users.noreply.github.com>
2022-01-04 12:50:22 +0100

    v1.1 - update baseurl

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox_ipcalculator',
-    version='1.0',
+    version='1.1',
     description='Sentia IP Calculator',
     url='https://github.com/PieterL75/netbox_ipcalculator',
     author='Pieter Lambrecht',

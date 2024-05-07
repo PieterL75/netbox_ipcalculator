0ab1479488c875aea4548325d2c46dac260248e1
Pieter Lambrecht <pieter.lambrecht@sentia.com>
2024-05-07 15:30:23 +0200

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import find_packages, setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='netbox_ipcalculator',
     version='1.4.0',
     description='Netbox IP Calculator',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     url='https://github.com/PieterL75/netbox_ipcalculator',
     author='Pieter Lambrecht',
     license='Apache 2.0',

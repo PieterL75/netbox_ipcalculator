cf34ee615b4e2561cbbeeea152500e60b93ecf88
PieterL75 <74899468+PieterL75@users.noreply.github.com>
2024-05-28 10:51:50 +0200

    Update navigation.py

--- netbox_ipcalculator/navigation.py
+++ netbox_ipcalculator/navigation.py
@@ -1,4 +1,8 @@
-from netbox.plugins import PluginMenuItem
+from netbox.settings import VERSION
+if VERSION.startswith("3."):
+    from extras.plugins import PluginMenuItem
+else:
+    from netbox.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(

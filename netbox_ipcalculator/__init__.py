c2eaf7a01491f48ccce80e1b24cf7052440054f4
Pieter Lambrecht <pieter.lambrecht@sentia.com>
2024-06-05 16:09:41 +0200

    v1.4.3

--- netbox_ipcalculator/__init__.py
+++ netbox_ipcalculator/__init__.py
@@ -8,7 +8,7 @@ class IPCalcultorConfig(PluginConfig):
     name = 'netbox_ipcalculator'
     verbose_name = 'IP Calculator'
     description = 'Netbox IP Calculator'
-    version = '1.4.2'
+    version = '1.4.3'
     author = 'Pieter Lambrecht'
     author_email = 'pieter.lambrecht@gmail.com'
     base_url = 'netbox_ipcalculator'

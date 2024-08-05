138b0e1f6825f9c83df8a4635f61bbf2e2d0788b
Pieter Lambrecht <pieter.lambrecht@accenture.com>
2024-08-05 14:41:15 +0200

    v1.4.5 FIX #15 Local jQuery

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
 from setuptools import find_packages, setup
+import codecs
+import os.path
 
 # read the contents of your README file
 from pathlib import Path
 
-from PieterL75.netbox_ipcalculator.pluginvars import (
-    __version__,
-    __name__,
-    __description__,
-    __author__,
-    __author_email__,
-    __url__,
-)
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
 
-long_description = __description__
+pluginvars={}
+
+for line in read('netbox_ipcalculator/pluginvars.py').splitlines():
+    if line.startswith('__'):
+        _delim = '"' if '"' in line else "'"
+        _data=line.split(_delim)
+        pluginvars[_data[0]]=_data[1]
+
+long_description = getattr(pluginvars, '__description__', '')
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name=__name__,
-    version=__version__,
-    description=__description__,
+    name=getattr(pluginvars,'__name__',''),
+    version=getattr(pluginvars,'__version__',''),
+    description=getattr(pluginvars,'__description__',''),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=__url__,
-    author=__author__,
+    url=getattr(pluginvars,'__url__',''),
+    author=getattr(pluginvars,'__author__',''),
     license='Apache 2.0',
     install_requires=[],
     packages=find_packages(),
